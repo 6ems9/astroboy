8dc38b6767c49e11d647b12e619c88f8a2de90ee
erick <esinaga11@gmail.com>
2026-09-24 20:47:54 +0700

    update index to use dynamic content (About&Resume)

---
+++ src/data/about_resume.ts
@@ -0,0 +1,70 @@
+export const contactInfo = {
+  image: "https://ui-avatars.com/api/?name=Astro+Boy&background=c084fc&color=fff&size=150",
+  name: "Astro Boy",
+  role: "Full-Stack Developer",
+  phone: "+62 812 3456 7890",
+  email: "astroboy@domain.com",
+  location: "Bogor, ID",
+};
+
+export const socialMedia = {
+  instagram: "https://instagram.com/yourusername",
+  facebook: "https://facebook.com/yourusername",
+  github: "https://github.com/yourusername",
+};
+
+export const about = {
+  description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, nisl vel tincidunt lacinia, nunc est ultricies nunc, ut lacus nunc nisl vel nunc. Donec euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, ut lacus nunc nisl vel nunc. Donec euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, ut lacus nunc nisl vel nunc. Donec euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, ut lacus nunc nisl vel nunc. Donec euismod, nisl vel tincidunt lacinia, nunc est ultricies nunc, ut lacus nunc nisl vel nunc.",
+  skills: [
+    "Front-End",
+    "Back-End",
+    "UI/UX Design",
+    "Full-Stack",
+    "Moibele Apps",
+    "DevOps",
+  ],
+  experience: "3+",
+  project: "20+",
+};
+
+export const experience = [
+  {
+    position: "Full-Stack Web Developer",
+    period: "2022 - present",
+    description:
+      "Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.",
+  },
+  {
+    position: "Web Designer",
+    period: "2020 — 2022",
+    description:
+      "Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.",
+  },
+  {
+    position: "Freelance Web Developer",
+    period: "2017 — 2020",
+    description:
+      "Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.",
+  },
+];
+
+export const education = [
+  {
+    school: "National University of Technology",
+    period: "2013 - 2017",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    school: "Jakarta Academy of Design",
+    period: "2010 – 2011",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    school: "Senior High School",
+    period: "2007 — 2010",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
