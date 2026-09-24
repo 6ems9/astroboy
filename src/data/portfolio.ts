a3dd79cacdd7c4be2d4bd377a4d3839e7dcfcaa9
erick <esinaga11@gmail.com>
2026-09-24 21:47:29 +0700

    update portfolio

---
+++ src/data/portfolio.ts
@@ -0,0 +1,38 @@
+export const portfolioItems = [
+  {
+    title: "E-Commerce Platform",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "#",
+    image: "https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?w=400&h=300&fit=crop",
+  },
+  {
+    title: "SaaS Dashboard",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "#",
+    image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=400&h=300&fit=crop",
+  },
+  {
+    title: "Point of Sale (POS)",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "#",
+    image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=400&h=300&fit=crop",
+  },
+  {
+    title: "Company Profile",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "#",
+    image: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=400&h=300&fit=crop",
+  },
+  {
+    title: "Crypto Tracker",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "#",
+    image: "https://images.unsplash.com/photo-1518546305927-5a555bb7020d?w=400&h=300&fit=crop",
+  },
+  {
+    title: "Chat App",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link: "#",
+    image: "https://images.unsplash.com/photo-1611606063065-ee7946f0787a?w=400&h=300&fit=crop",
+  },
+];
